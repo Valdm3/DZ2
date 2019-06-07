8b20f794c15da84ea49ef233b841fc765a47f42b
Влад <@DESKTOP-9TFSDKB>
2019-06-07 18:40:52 +0300

    Всё

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -17,15 +17,28 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    
+    String[][] demoArray = new String [2][2];
+    
+    String[] row1 = {"X", "O", "X"};
+    demoArray[0] = row1;
+    
+    String[] row2 = {"O", "X", "O"};
+     demoArray[1] = row2;
+    
+        for (int i= 0; i < demoArray.length; i++) {
+             for (int j = 0; j < demoArray[i].length; j++)
+        System.out.printf("%2s",demoArray[i][j]);
+    }
+       System.out.println();
+    
         
-        // int x = 100500;
-        // long y = 4543658768979089L;
         
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input x (long): ");
-        long x = sc.nextLong();
         
-        System.out.println(x);
+        
+        
+                
+       
     }
     
 }
